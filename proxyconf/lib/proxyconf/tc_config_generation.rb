2fc17111be4deaa333757da48631f40afe8f97ed
Bartosz <bwilk@bw-thinkpad.(none)>
2012-11-08 09:30:10 +0100

    Introduced tests for nginx configuration generation and provided method to remove address from all services

---
+++ proxyconf/lib/proxyconf/tc_config_generation.rb
@@ -0,0 +1,60 @@
+require_relative './config_generation'
+require "test/unit"
+
+module ProxyConf
+    class TestStringGeneration < Test::Unit::TestCase
+
+	def test_generation
+	    #given 
+	    context = "context123"
+	    app = "app123"
+	    service = "service111"
+	    service2 = "service222"
+	    address = "www.google.pl"
+	    address2 = "www.google2.pl"
+	    proxy_timeout = 10
+	    proxy_send_timeout = 10
+	    
+	    contexts = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = [] } } }
+	    contexts[context][app][service] << address;
+	    contexts[context][app][service2] << address;
+	    contexts[context][app][service] << address2;
+	    
+	    generation = ConfigGeneration.new(contexts, proxy_timeout, proxy_send_timeout)
+	    
+	    #then	    
+	    upstream_config, proxy_config = generation.generate
+
+	    assert(upstream_config.gsub(/[\s\n]/, "").include?("
+	    upstream ctx.#{context}.app.#{app}.service.#{service} { 
+		server #{address};  
+		server #{address2}; 
+	    }
+	    ".gsub(/[\s\n]/, "")));
+	    
+	    assert(upstream_config.gsub(/[\s\n]/, "").include?("
+	    upstream ctx.#{context}.app.#{app}.service.#{service2} { 
+		server #{address}; 
+	    }
+	    ".gsub(/[\s\n]/, "")));
+	    
+	    assert(proxy_config.gsub(/[\s\n]/, "").include?"
+	    location \/#{context}\/#{app}\/#{service}\/ {
+		proxy_read_timeout #{proxy_timeout};
+	        proxy_send_timeout #{proxy_send_timeout};
+		proxy_pass http:\/\/ctx.#{context}.app.#{app}.service.#{service}\/;
+	        proxy_set_header X-Path-Prefix \"/#{app}/#{service}\";
+	    }   
+	    ".gsub(/[\s\n]/, ""));
+	    
+	    assert(proxy_config.gsub(/[\s\n]/, "").include?"
+	    location \/#{context}\/#{app}\/#{service2}\/ {
+		proxy_read_timeout #{proxy_timeout};
+	        proxy_send_timeout #{proxy_send_timeout};
+		proxy_pass http:\/\/ctx.#{context}.app.#{app}.service.#{service2}\/;
+	        proxy_set_header X-Path-Prefix \"/#{app}/#{service2}\";  
+	    }".gsub(/[\s\n]/, "")); 
+	end
+	
+    end
+end 
---
+++ proxyconf/lib/proxyconf/tc_nginx_configuration.rb
@@ -0,0 +1,118 @@
+require_relative './nginx_configuration'
+require "test/unit"
+
+module ProxyConf
+    class TestNginxConfiguration < Test::Unit::TestCase
+	
+	def setup
+	    @context = "context123"
+	    @app = "app123"
+	    @service = "service111"
+	    @service2 = "service222"
+	    @address = "www.google.pl"
+	    @address2 = "www.google2.pl"
+	end
+	
+	def test_register
+	    #given 
+	    conf = NginxConfiguration.new(nil)	    
+	    #when 
+	    conf.register(@context, @app, @service, @address) 
+	    #then
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	end
+	
+	def test_register_two
+	    #given 
+	    conf = NginxConfiguration.new(nil)	    
+	    #when 
+	    conf.register(@context, @app, @service, @address) 
+	    conf.register(@context, @app, @service, @address2) 
+	    #then
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	end
+ 	
+	def test_unregister
+	    #given 
+	    conf = NginxConfiguration.new(nil)
+	    conf.register(@context, @app, @service, @address)
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	    #when 
+	    conf.unregister(@context, @app, @service, @address)
+	    #then	    
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	end
+	
+	def test_unregister_one_leave_rest
+	    #given 
+	    conf = NginxConfiguration.new(nil)
+	    conf.register(@context, @app, @service, @address)
+	    conf.register(@context, @app, @service, @address2)
+	    conf.register(@context, @app, @service2, @address)
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	    assert(conf.is_registered(@context, @app, @service2, @address))
+	    #when 
+	    conf.unregister(@context, @app, @service, @address)
+	    #then
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	    assert(conf.is_registered(@context, @app, @service2, @address))
+	end
+	
+	def test_unregister_nothing
+	    #given 
+	    conf = NginxConfiguration.new(nil)
+	    #when 
+	    conf.unregister(@context, @app, @service, @address)
+	    #then	    
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	end
+	
+	def test_unregister_from_all
+	    #given
+	    conf = NginxConfiguration.new(nil)
+	    conf.register(@context, @app, @service, @address)
+	    conf.register(@context, @app, @service2, @address)
+	    conf.register(@context, @app, @service, @address2)
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	    assert(conf.is_registered(@context, @app, @service2, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	    #when 
+	    conf.unregister_from_all(@address)
+	    #then 
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	    assert(!conf.is_registered(@context, @app, @service2, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	end
+	
+	def test_unregister_two_from_all
+	    #given
+	    conf = NginxConfiguration.new(nil)
+	    conf.register(@context, @app, @service, @address)
+	    conf.register(@context, @app, @service2, @address)
+	    conf.register(@context, @app, @service, @address2)
+	    assert(conf.is_registered(@context, @app, @service, @address))
+	    assert(conf.is_registered(@context, @app, @service2, @address))
+	    assert(conf.is_registered(@context, @app, @service, @address2))
+	    #when 
+	    conf.unregister_from_all(@address)
+	    conf.unregister_from_all(@address2)
+	    #then 
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	    assert(!conf.is_registered(@context, @app, @service2, @address))
+	    assert(!conf.is_registered(@context, @app, @service, @address2))
+	end
+	
+	def test_unregister_nothing_from_all
+	    #given
+	    conf = NginxConfiguration.new(nil)
+	    #when 
+	    conf.unregister_from_all(@address)
+	    #then 
+	    assert(!conf.is_registered(@context, @app, @service, @address))
+	end
+
+    end
+end 
